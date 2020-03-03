17d4fb797760d53290270d04deec10a3f6cc2efd
Abhishek Kadam <abhimkdm@gmail.com>
2020-03-03 10:03:43 +0530

    Class & Object
    Class & Object

---
+++ OOPSSession/OOPSSession/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSSession
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person person = new Person("Prsannna");
+            person.Display();
+
+            Console.ReadKey();
+        }
+
+        public class Person {
+            public string Name;
+
+            public Person(string name) {
+                this.Name = name;
+            }
+
+            public void Display() {
+                Console.WriteLine("Welcome," + Name);
+            }
+        }
+    }
+}
