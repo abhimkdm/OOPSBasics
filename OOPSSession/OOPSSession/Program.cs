41a44f96663bd9545d9f7d122fe3ef15a7730c43
Abhishek Kadam <abhimkdm@gmail.com>
2020-03-04 08:10:55 +0530

    Inheritence, Polymorphism, Interface, Abstraction, Encapsulation, Access Modiffiers

--- OOPSSession/OOPSSession/Program.cs
+++ OOPSSession/OOPSSession/Program.cs
@@ -10,21 +10,46 @@ namespace OOPSSession
     {
         static void Main()
         {
-            Person person = new Person("Prsannna");
+            IPerson person = new Person("Prsannna","p@gmail.com",989898989);
             person.Display();
+            person.Display("Implemented Display Method");
 
             Console.ReadKey();
         }
 
-        public class Person {
+        public interface IPerson {
+           void Display(string text);
+           void Display();
+        }
+
+        public class ContactDetails {
+            public string Email;
+            protected int MobileNo;
+            private string Password;
+
+            public ContactDetails() {
+                //this.Password
+            }
+        }
+        public class Person : ContactDetails, IPerson
+        {
             public string Name;
 
-            public Person(string name) {
+            public Person(string name, string email, int mobno) {
                 this.Name = name;
+                this.Email = email;
+                this.MobileNo = mobno;
             }
 
             public void Display() {
                 Console.WriteLine("Welcome," + Name);
+                Console.WriteLine("Email," + Email);
+                Console.WriteLine("MobNo," + MobileNo);
+            }
+
+            public void Display(string text)
+            {
+                Console.WriteLine(text);
             }
         }
     }
